1dd90eb691277035904b2c70f6130dfc3dd410f8
jasonsmithj <json.smith.jp@gmail.com>
2020-04-23 01:02:55 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app dark>
+    <div id="app">
+      <v-content>
+        <v-container>
+          <nuxt />
+        </v-container>
+      </v-content>
+    </div>
+  </v-app>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-layout align-center justify-center>
+    <v-flex xs24 md24 text-xs-center>
+      <v-card flat >
+        <v-card-title class="justify-center">
+          <h2>Gateway IP: {{ origin }}</h2>
+        </v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+  import axios from 'axios'
+
+  @Component({
+    async asyncData() {
+      const res = await axios.get("https://httpbin.org/ip")
+      return res.data
+    }
+  })
+  export default class GetGatewayIP extends Vue {
+  }
+</script>
